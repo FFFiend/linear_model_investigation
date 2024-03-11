e47342b9f714a9f44830ce38d769344d716d1471
FFFiend <owais.zahid@mail.utoronto.ca>
2024-03-11 03:47:13 -0500

    data section for paper

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -20,5 +20,5 @@ ces2020 <-
   ) |>
   select(votereg, CC20_410, gender, educ, race, birthyr)
 
-write_csv(ces2020, "~/linear_model_investigation/data/ces2020.parquet")
+write_csv(ces2020, "~/linear_model_investigation/data/ces2020_raw.parquet")
 
